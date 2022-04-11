ff6332a83a6f7d97d1dd989fd0c99e03eaea50db
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-11 15:08:30 +0200

    Struttura documento

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+/* Key Point:
+  1. Creare una lista di utenti autorizzati (array)
+  2. Permetti solo agli utenti autorizzati di accedere (condition if)
+  3. Stampa un messaggio appropriato sull’esito del controllo (true/false)
+  4. Una volta loggato invitiamo l'utente a giocare con i dadi.
+  5. Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+  6. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
