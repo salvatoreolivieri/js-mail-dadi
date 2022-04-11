cbab1c588c3de383f2a25dc22ff41457350bd11e
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-11 17:32:32 +0200

    Prima prova ciclo per confrontare la mail di input con la mail dell'array

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
   3. Stampa un messaggio appropriato sull’esito del controllo (true/false)
   4. Una volta loggato invitiamo l'utente a giocare con i dadi.
   5. Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-  6. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+  6. StabilKKire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 const user_authorized = [
@@ -16,22 +16,24 @@ const user_authorized = [
 ]
 
 const btn_autentication = document.querySelector("#autentication");
-
-
 console.log(btn_autentication);
 
 btn_autentication.addEventListener("click", function() {
 
-  let user_mail = document.getElementById('mail').value;  
-
+  const user_mail = document.getElementById('mail').value;
   console.log(user_mail);
 
+  for (let n = 0; n < user_authorized.length; n++){
+  
+    let mail_presente = false;
+  
+    document.getElementById("output").innerHTML = "Non hai l'autorizzazione per giocare";
+  
+    if (user_mail === user_authorized[n]){
+      document.getElementById("output").innerHTML = "Ci siamo, ora sei pronto a giocare";
+    }
+  
+  }
+  
 });
 
-
-
-if (!(user_mail === user_authorized)){
-  document.getElementById("output").innerHTML("Non hai l'autorizzazione per accedere")
-} else {
-  document.getElementById("output").innerHTML("Ci siamo, ora sei pronto a giocare")
-}
