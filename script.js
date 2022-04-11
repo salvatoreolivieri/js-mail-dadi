117e157e2faa0001191762cce864002bcb29bda7
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-11 22:01:49 +0200

    Cambiato il modo di collezionare la mail dell'utente (ma continua a non funzionare il messaggio stampato in pagina)

--- script.js
+++ script.js
@@ -9,31 +9,50 @@
   6. StabilKKire il vincitore, in base a chi fa il punteggio più alto.
 */
 
+
+// lista utenti autorizzati
 const user_authorized = [
   "Salvatore.olivieri07@gmail.com",
   "Luca.golinelli@gmail.com",
   "Stefano.cavazza@gmail.com"
 ]
 
-const btn_autentication = document.querySelector("#autentication");
-console.log(btn_autentication);
+// raccolgo la mail dell'utente
 
-btn_autentication.addEventListener("click", function() {
+const user_mail = prompt("Inserisci la tua mail per giocare");
 
-  const user_mail = document.getElementById('mail').value;
-  console.log(user_mail);
 
-  for (let n = 0; n < user_authorized.length; n++){
+/* eliminata perchè con questo non mi funzionava */
+// const btn_autentication = document.querySelector("#autentication");
+// console.log(btn_autentication);
+
+// btn_autentication.addEventListener("click", function() {
+
+//   const user_mail = document.getElementById('mail').value;
+//   console.log(user_mail);
+
+// });
+
+
+
+
+for (let x = 0; x < user_authorized.length; x++){
   
-    let mail_presente = false;
-  
-    document.getElementById("output").innerHTML = "Non hai l'autorizzazione per giocare";
-  
-    if (user_mail === user_authorized[n]){
-      document.getElementById("output").innerHTML = "Ci siamo, ora sei pronto a giocare";
-    }
-  
+  let mail_presente = false;
+
+  if (user_authorized[x] === user_mail){
+    mail_presente = true;
+    document.getElementById("output").innerHTML = "Ci siamo, ora sei pronto a giocare";
+
+    console.log(mail_presente);
+
+  } else{
+    document.getElementById("output").innerHTML = "Attenzione: Non hai l'autorizzazione!";
   }
-  
-});
+}
+
+// if (mail_presente = true){
+// } else{
+//   document.getElementById("output").innerHTML = "Attenzione: Non hai l'autorizzazione!";
+// }
 
