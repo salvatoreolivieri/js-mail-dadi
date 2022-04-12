a25de7ebd31b19a19830830fc4bdc487b075473e
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-12 12:12:55 +0200

    Autentificazione FINALMENTE funzionante

--- script.js
+++ script.js
@@ -22,52 +22,18 @@ const user_authorized = [
 const user_mail = prompt("Inserisci la tua mail per giocare");
 
 
-/* eliminata perchè con questo non mi funzionava */
-// const btn_autentication = document.querySelector("#autentication");
-// console.log(btn_autentication);
+let correct_mail = false; 
 
-// btn_autentication.addEventListener("click", function() {
-
-//   const user_mail = document.getElementById('mail').value;
-//   console.log(user_mail);
-
-// });
-
-let mail_presente = false;
-
-for (let x = 0; x < user_authorized.length; x++){
+for (let x = 0; x < user_authorized.length; x++) {
 
   if (user_authorized[x] === user_mail){
-    mail_presente = true;
-  }
+    correct_mail = true;
+  } 
+
 }
 
-
-if (mail_presente = false){
-  document.getElementById("output").innerHTML = "Attenzione: Non hai l'autorizzazione!";
-} else{
-  document.getElementById("output").innerHTML = "Ci siamo, ora sei pronto a giocare";
+if (correct_mail === true) {
+  console.log("sei autorizzato")
+} else {
+  console.log("non sei autorizzato");
 }
-
-
-// Gioco dei dadi
-
-
-
-// const bottone_dadi = document.getElementById("#bottone-dadi");
-// // const bottone_rilancia = document.getElementById("#bottone-rilancia");
-
-// // console.log(bottone_rilancia);
-
-// const container_risultati_dadi = document.getElementById("#container_risultati_dadi");
-
-// // coccnsole.log(container_risultati_dadi);
-
-// bottone_dadi.addEventListener("click", function(){
-
-//   container_risultati_dadi.classList.remove("hide");
-//   bottone_rilancia.classList.remove("hide");
-
-
-// });
-
